d80ea04e9f3560fa0204800da7a2f8f59448c2be
Royi Reshef <royi1000@gmail.com>
2017-05-22 23:58:35 +0300

    continue json parsing

--- api/get_json.py
+++ api/get_json.py
@@ -2,7 +2,8 @@ import os
 import logging
 import json
 import requests
-from collection import defaultdict
+import argparse
+from collections import defaultdict
 from colorlog import ColoredFormatter
 
 logging.basicConfig()
@@ -14,6 +15,7 @@ native_comment = []
 #               "firstSection": "Biur Halacha 1:1",
 #                "base_text_mapping": "commentary_increment_base_text_depth",
 #                "title": "Biur Halacha",
+
 
 def sef_logger():
     color_formatter = ColoredFormatter("%(log_color)s %(asctime)s - %(name)s - %(levelname)s - %(message)s%(reset)s")
@@ -31,6 +33,58 @@ class TOC(object):
     NO_TEXT = "with_text=0"
     NO_COMMENT = "commentary=0"
     ALL_BOOK = "pad=0"
+    comment_handle = {'Tafsir Rasag': 'one_off_introduction',
+                      'Rabbeinu Bahya': 'one_off_introduction',
+                      'Iben Ezra on Lamentations': 'one_off_introduction',
+                      'Ralbag on Song of Songs': 'one_off_introduction',
+                      'Tur HaAroch': 'one_off_introduction',
+                      'Tosafot Yom Tov': 'one_off_introduction_check', # not always introduction
+                      'Targum of I Chronicles': 'ignore',
+                      'Targum of II Chronicles': 'ignore',
+                      'Targum Neofiti': 'neofiti',  # only first genesis chapter,
+                      # 'Abarbanel on Torah': 'one_to_one',
+                      # 'Bekhor Shor': 'one_to_one',
+                      # 'Chizkuni': 'one_to_one',
+                      # 'Minchat Shai': 'one_to_one',
+                      'Ralbag': 'one_to_one',
+                      # 'Gra on Pirkei Avot': 'one_to_one',
+                      # 'Lechem Shamayim': 'one_to_one',
+                      # 'Rabbeinu Yonah on Pirkei Avot': 'one_to_one',
+
+                      # 'Rif': 'one_to_one',
+                      # 'Rashba': 'one_to_one',
+                      # 'Chidushei Agadot': 'one_to_one',
+                      # 'Chidushei Halachot': 'one_to_one',
+                      # 'Maharam Shif': 'one_to_one',
+                      # 'Pilpula Charifta': 'one_to_one',
+                      # 'Ramban': 'one_to_one',
+                      # 'Ritva': 'one_to_one',
+                      # 'Rosh': 'one_to_one',
+                      # 'Shita Mekubetzet': 'one_to_one',
+                      'Biur Halacha': 'one_to_one',
+
+                      'Gur Aryeh': 'one_to_one_gur',
+                      'Harchev Davar': 'one_to_one_gur',
+                      'Siftei Hakhamim': 'one_to_one_gur',
+                      'JPS': 'not_direct',
+                      'Meshech Hochma': 'not_direct',
+                      'Penei David': 'not_direct',
+                      'Akeidat Yitzchak': 'not_direct',
+                      'Rambam Introduction to Masechet Horayot': 'not_direct',
+                      'Rambam Introduction to Seder Kodashim': 'not_direct',
+                      'Boaz': 'not_direct',
+                      'Yachin': 'not_direct',
+                      'Divrey Chamudot': 'not_direct',
+                      'Korban Netanel': 'not_direct',
+                      'Maadaney Yom Tov': 'not_direct',
+                      'Tiferet Shmuel': 'not_direct',
+                      'Yad Ramah': 'not_direct',
+                      'Commentary on Sefer Hamitzvot of Rasag': 'not_direct',
+                      'Hasagot HaRamban al Sefer HaMitzvot': 'not_direct',
+
+                      'Shney Luchot HaBrit': 'complex_shnei',  # parash and paragraph (Shney Luchot HaBrit, Bereshit, Derech Chaim Tochachot Musar)
+
+                      }
 
     def __init__(self, host=DEFAULT_HOST):
         self.host = host
@@ -40,6 +94,28 @@ class TOC(object):
         self.comment_map = defaultdict(list)
         self.counter = 1
         self.json_dir = '/tmp/json'
+
+    def check(self, items):
+        for item in items:
+            if "category" in item:
+                self.check(item[u'contents'])
+            else:
+                #  item title have priority
+                if (item.get('collectiveTitle') not in self.comment_handle) and (item["title"] not in self.comment_handle):
+                    if "base_text_mapping" in item:
+                        if item["base_text_mapping"]:
+                            if item["base_text_titles"][0].replace('_', ' ') not in item["firstSection"]:
+                                logger.warning("item: {}, base:{} not in first: {}".
+                                               format(item['title'], item["base_text_titles"][0], item["firstSection"]))
+                                raise
+                        else:
+                            logger.info("no base mapping item: {} -- first:{} ++ titles:{}".
+                                        format(item['title'],
+                                               item["firstSection"],item.get("base_text_titles")))
+                            f = item["firstSection"]
+                            if ':' not in f[-3:] and not f[-1] == '1' and \
+                                    not (f[-2].isdigit() and (f[-1] in ['a', 'b'])):
+                                raise
 
     def walk_on_toc(self, toc_file_name="toc.xml"):
         if not os.path.exists(self.json_dir):
@@ -53,6 +129,9 @@ class TOC(object):
         for item in items:
             if "category" in item:
                 logger.info("category: {} {}".format(item["category"], item["heCategory"].encode('utf-8')))
+                if item['category'] in ['Targum Neofiti']:
+                    logger.warning("skipped: {}".format(item['category']))
+                    continue
                 self.toc_file.write('{}<node n="{}" en="{}">\n'.format(' ' * 4 * level,
                                                                        item[u'heCategory'].
                                                                        encode('utf-8').replace('"', "''"),
@@ -61,22 +140,63 @@ class TOC(object):
                 self.toc_file.write('{}</node>\n'.format(' ' * 4 * level))
             else:  # real book
                 title, he_title = item["title"], item["heTitle"]
-                if not item["dependence"]:
+                if not item["dependence"]:  # not commentary
+                    continue
                     book = self.get_book(item["firstSection"], chapter=None)
                     if book["isComplex"]:
                         self.handle_complex_book(book, level)
                     else:
-                        book = self.get_book(item["title"], all=True)
+                        book = self.get_book(title, all=True)
                         self.handle_simple_book(book, level)
                 else:
-                    if title in native_comment:
-                        for b in book["base_text_titles"]:
-                            self.map[b].append(title)
+                    book = self.get_book(item["firstSection"], chapter=None)
+                    if item["base_text_titles"]:
+                        base_text_titles = item["base_text_titles"]
+                        if title in ['Targum of I Chronicles', 'Targum of II Chronicles']:
+                            base_text_titles = [title.split('of')[1][1:]]
+
+                        if title in ['Akeidat Yitzchak', 'Penei David', 'JPS',
+                                     'Penei David']:  # not direct commentary
+                            if book["isComplex"]:
+                                self.handle_complex_book(book, level)
+                            else:
+                                book = self.get_book(title, all=True)
+                                self.handle_simple_book(book, level)
+                            for base in base_text_titles:
+                                self.comment_map[base].append((title, 'not_direct'))
+                            continue
+
+                        if len(base_text_titles) > 1 and title != item['collectiveTitle']:
+                            logger.info("category: {} {}".format(title, he_title.encode('utf-8')))
+                            self.toc_file.write('{}<node n="{}" en="{}">\n'.format(' ' * 4 * level,
+                                                                                   he_title.
+                                                                                   encode('utf-8').replace('"', "''"),
+                                                                                   title))
+                        first_section = item['firstSection']
+                        if first_section.endswith('Introduction'):
+                            self.handle_simple_book(book, level+1)
+                            first_section = book['next']
+
+                        if not item.get('base_text_mapping') and item['firstSection'].endswith('1:1'):
+                            item["base_text_mapping"] = "many_to_one"
+                        assert (item["base_text_mapping"])
+                        if base_text_titles[0] in first_section:
+                            start = first_section.split(base_text_titles[0])[0]
+                            for base in base_text_titles:
+                                self.comment_map[base].append((title, item['base_text_mapping']))
+                                book = self.get_book("{}{}".format(start, base), all=True)
+                                self.handle_simple_book(book, level+1)
+                        else:
+                            raise item
+
+                        if len(base_text_titles) > 1 and title != item['collectiveTitle']:
+                            self.toc_file.write('{}</node>\n'.format(' ' * 4 * level))
+
                     #import pdb; pdb.set_trace()
                     else:
                         logger.error("unknown commentary type: {} firstSection: {} base_text_mapping".
                                      format(title, item["firstSection"], item["base_text_mapping"]))
-                self.counter += 1
+                        raise item
 
     def handle_complex_book(self, book, level):
         length = 0
@@ -88,7 +208,7 @@ class TOC(object):
             book_json['chapter_en'].append(book['book'])
             book_json['chapter_he'].append(book['heTitle'])
             book_json['he'].append(book['he'])
-            book_json['en'].append(book['en'])
+            book_json['en'].append(book.get('en', []))
             if not book['next']:
                 book = None
             else:
@@ -98,8 +218,9 @@ class TOC(object):
                         format(' ' * 4 * level,
                                he_title.encode('utf-8').replace('"', "''"),
                                title, self.counter, length))
-        self.map[book['text']: self.counter]
+        self.map[book['text']] = self.counter
         self.write_book(book_json, self.counter)
+        self.counter += 1
 
     def handle_simple_book(self, book, level):
         book_json = {}
@@ -126,8 +247,9 @@ class TOC(object):
                             format(' ' * 4 * level,
                                    he_title.encode('utf-8').replace('"', "''"),
                                    title, self.counter, length))
-        self.map[book['text']: self.counter]
+        self.map[title] = self.counter
         self.write_book(book_json, self.counter)
+        self.counter += 1
 
     def write_book(self, data, _id):
         open('{}/{}.json'.format(self.json_dir, _id), 'w+').write(json.dumps(data, indent=1))
@@ -151,6 +273,13 @@ class TOC(object):
         return json.loads(requests.get(url).text)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--check", help="check validity of direct commenters",
+                        action="store_true", dest="check")
+    args = parser.parse_args()
     sef_logger()
     toc = TOC()
-    toc.walk_on_toc()
+    if args.check:
+        toc.check(toc.toc)
+    else:
+        toc.walk_on_toc()
